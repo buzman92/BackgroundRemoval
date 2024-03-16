f822f1f05c40565ba50be8e859b69792db511b45
buzman92 <13799555+buzman92@users.noreply.github.com>
2024-03-16 16:28:24 +0000

    update filesize limit

--- bg_remove.py
+++ bg_remove.py
@@ -4,15 +4,17 @@ from PIL import Image
 from io import BytesIO
 import base64
 
-st.set_page_config(layout="wide", page_title="Image Background Remover")
+st.set_page_config(layout="wide", page_title="Image BG Remover by Bz")
 
 st.write("## Remove background from your image")
 st.write(
     ":dog: Try uploading an image to watch the background magically removed. Full quality images can be downloaded from the sidebar. This code is open source and available [here](https://github.com/tyler-simons/BackgroundRemoval) on GitHub. Special thanks to the [rembg library](https://github.com/danielgatis/rembg) :grin:"
 )
+st.sidebar.write("## BG Remover by Buzman")
+
 st.sidebar.write("## Upload and download :gear:")
 
-MAX_FILE_SIZE = 5 * 1024 * 1024  # 5MB
+MAX_FILE_SIZE = 50 * 8192  * 8192  # 50MB
 
 # Download the fixed image
 def convert_image(img):
@@ -39,7 +41,7 @@ my_upload = st.sidebar.file_uploader("Upload an image", type=["png", "jpg", "jpe
 
 if my_upload is not None:
     if my_upload.size > MAX_FILE_SIZE:
-        st.error("The uploaded file is too large. Please upload an image smaller than 5MB.")
+        st.error("The uploaded file is too large. Please upload an image smaller than 50MB.")
     else:
         fix_image(upload=my_upload)
 else:
